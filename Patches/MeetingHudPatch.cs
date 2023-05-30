8cbce9dc218a63eb9c21c3d480eff1256c4ebb52
Loonie <51543683+Loonie-Toons@users.noreply.github.com>
2023-05-30 12:33:56 -0400

    Guesser Mode: Player IDs fixed

--- Patches/MeetingHudPatch.cs
+++ Patches/MeetingHudPatch.cs
@@ -743,7 +743,7 @@ class MeetingHudStartPatch
                 if (Options.NeutralKillersCanGuess.GetBool() && seer.GetCustomRole().IsNK())
                     if (!seer.Data.IsDead && !target.Data.IsDead)
                     pva.NameText.text = Utils.ColorString(Utils.GetRoleColor(seer.GetCustomRole()), target.PlayerId.ToString()) + " " + pva.NameText.text;
-                if (Options.PassiveNeutralsCanGuess.GetBool() && !seer.GetCustomRole().IsNonNK())
+                if (Options.PassiveNeutralsCanGuess.GetBool() && seer.GetCustomRole().IsNonNK())
                     if (!seer.Data.IsDead && !target.Data.IsDead)
                     pva.NameText.text = Utils.ColorString(Utils.GetRoleColor(seer.GetCustomRole()), target.PlayerId.ToString()) + " " + pva.NameText.text;
 
