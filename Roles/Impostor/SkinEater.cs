6f0990f5cb71896d0223391302fd65fd581a608e
lars-wu <papercut92@hotmail.de>
2023-06-05 17:47:53 +0200

    class added

---
+++ Roles/Impostor/SkinEater.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static TOHE.Options;
+using static TOHE.Translator;
+
+namespace TOHE.Roles.Impostor
+{
+    internal static class SkinEater
+    {
+    }
+}
