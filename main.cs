0aece30d4f4476ad3acc581cee0552e4b921df10
Loonie <51543683+Loonie-Toons@users.noreply.github.com>
2023-06-09 23:14:07 -0400

    v2.4.0 Pre-release 1 (4/4)

--- main.cs
+++ main.cs
@@ -24,7 +24,7 @@ namespace TOHE;
 public class Main : BasePlugin
 {
     // == プログラム設定 / Program Config ==
-    public static readonly string ModName = "TOHE";
+    public static readonly string ModName = "TOHE-R";
     public static readonly string ModColor = "#ffc0cb";
     public static readonly bool AllowPublicRoom = true;
     public static readonly string ForkId = "TOHE";
