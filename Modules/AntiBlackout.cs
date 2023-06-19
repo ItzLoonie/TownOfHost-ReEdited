8285ba74c50993910ec06187048c9a5539e64487
Tommy XL <fadeevvladimir16@gmail.com>
2023-06-19 23:42:30 +0800

    Add Hex Master

--- Modules/AntiBlackout.cs
+++ Modules/AntiBlackout.cs
@@ -21,7 +21,7 @@ public static class AntiBlackout
     ///<summary>
     ///AntiBlackout内の処理が必要であるかどうか
     ///</summary>
-    public static bool IsRequired => Options.NoGameEnd.GetBool() || Jackal.IsEnable || Pelican.IsEnable || Gamer.IsEnable || BloodKnight.IsEnable || Succubus.IsEnable || Poisoner.IsEnable || Infectious.IsEnable || Juggernaut.IsEnable || Virus.IsEnable || Wraith.IsEnable || CustomRoles.NSerialKiller.IsEnable();
+    public static bool IsRequired => Options.NoGameEnd.GetBool() || Jackal.IsEnable || Pelican.IsEnable || Gamer.IsEnable || BloodKnight.IsEnable || Succubus.IsEnable || Poisoner.IsEnable || Infectious.IsEnable || Juggernaut.IsEnable || Virus.IsEnable || Wraith.IsEnable || HexMaster.IsEnable || CustomRoles.NSerialKiller.IsEnable();
     ///<summary>
     ///インポスター以外の人数とインポスターの人数の差
     ///</summary>
