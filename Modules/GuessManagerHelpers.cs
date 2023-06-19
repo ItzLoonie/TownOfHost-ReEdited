af7d05047518599a68678529ceabdc2fb3162519
xVCantCode <95es2017@gmail.com>
2023-06-19 17:49:26 +0300

    Removal of devs (temporary) Individual guess settings (*When CanGuessAddon is off it acts like its on, needs fix*) Optionholder update
    More string options for English.dat
    
    update max /level
    fix /kick returning a ban
    
    string update
    
    remove duplicate using!

---
+++ Modules/GuessManagerHelpers.cs
@@ -0,0 +1,58 @@
+﻿using TOHE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal static class GuessManagerHelpers
+{
+    public static Dictionary<CustomRoles, OptionItem> GuessOptionTarget = new Dictionary<CustomRoles, OptionItem>();
+
+    public static readonly string[] GuessOption = { "CanGuessAll", "CanGuessIndividual" };
+
+    public static void SetUpSpecialOptions(int Id)
+    {
+        foreach (var special in Enum.GetValues(typeof(CustomRoles)).Cast<CustomRoles>().Where(x => x.IsAdditionRole() && x is not CustomRoles.KB_Normal))
+        {
+            SetUpSpecialAbilityOption(special, Id, true, Options.GuessAddonExemptions);
+            Id++;
+        }
+    }
+
+    public static void SetUpSpecialAbilityOption(CustomRoles role, int Id, bool defaultValue = true, OptionItem parent = null)
+    {
+        parent ??= Options.GuessAddonExemptions;
+        var roleName = Utils.GetRoleName(role);
+        Dictionary<string, string> replacementDic = new Dictionary<string, string>() { { "%role%", Utils.ColorString(Utils.GetRoleColor(role), roleName) } };
+        GuessOptionTarget[role] = BooleanOptionItem.Create(Id, "CanGuess%role%Addon", defaultValue, TOHE.TabGroup.GameSettings, false).SetParent(parent);
+        GuessOptionTarget[role].ReplacementDictionary = replacementDic;
+    }
+
+    public static bool CanGuessAddon(CustomRoles role)
+    {
+        if (role.IsAdditionRole())
+        {
+            if (Options.GuesserMode.GetBool())
+            {
+                if (!Options.CanGuessAddons.GetBool())
+                {
+                    if (role.GetCustomRoleTypes() == CustomRoleTypes.Impostor && !Options.ImpostorsCanGuess.GetBool())
+                        return false;
+                    if (role.GetCustomRoleTypes() == CustomRoleTypes.Crewmate && !Options.CrewmatesCanGuess.GetBool())
+                        return false;
+                    if (role.GetCustomRoleTypes() == CustomRoleTypes.Neutral && (!Options.NeutralKillersCanGuess.GetBool() || !Options.PassiveNeutralsCanGuess.GetBool()))
+                        return false;
+                }
+            }
+            else
+            {
+                if (role.GetCustomRoleTypes() == CustomRoleTypes.Impostor && !Options.EGCanGuessAdt.GetBool())
+                    return false;
+                if (role.GetCustomRoleTypes() == CustomRoleTypes.Crewmate && !Options.GGCanGuessAdt.GetBool())
+                    return false;
+                if (role.GetCustomRoleTypes() == CustomRoleTypes.Neutral && !Options.GCanGuessAdt.GetBool())
+                    return false;
+            }
+        }
+        return true;
+    }
+}
