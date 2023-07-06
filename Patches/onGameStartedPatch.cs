e68b39c7b6c20ef3487bf3893abd2c01463d8474
Tommy XL <fadeevvladimir16@gmail.com>
2023-07-06 14:25:01 +0800

    Fix error

--- Patches/onGameStartedPatch.cs
+++ Patches/onGameStartedPatch.cs
@@ -844,7 +844,7 @@ internal class SelectRolesPatch
         var allPlayers = new List<PlayerControl>();
         foreach (var pc in Main.AllPlayerControls)
         {
-            if (pc.Is(CustomRoles.GM) || (pc.HasSubRole() && pc.GetCustomSubRoles().Count >= Options.NoLimitAddonsNumMax.GetInt()) || pc.Is(CustomRoles.Ntr) || pc.Is(CustomRoles.God) || pc.Is(CustomRoles.FFF)) continue;
+            if (pc.Is(CustomRoles.GM) || (pc.HasSubRole() && pc.GetCustomSubRoles().Count >= Options.LimitAddonsNumMax.GetInt()) || pc.Is(CustomRoles.Ntr) || pc.Is(CustomRoles.God) || pc.Is(CustomRoles.FFF)) continue;
             allPlayers.Add(pc);
         }
         var role = CustomRoles.Lovers;
