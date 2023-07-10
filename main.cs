0d12a0de14368e2b21b7989114ef5121e49b79b8
Loonie <51543683+Loonie-Toons@users.noreply.github.com>
2023-07-10 01:14:31 -0400

    dev 3 -> dev 4

--- main.cs
+++ main.cs
@@ -33,7 +33,7 @@ public class Main : BasePlugin
     public static ConfigEntry<string> DebugKeyInput { get; private set; }
     public static readonly string MainMenuText = " ";
     public const string PluginGuid = "com.karped1em.townofhostedited";
-    public const string PluginVersion = "2.4.3.3";
+    public const string PluginVersion = "2.4.3.4";
     public const int PluginCreate = 3;
     public const bool Canary = false;
 
