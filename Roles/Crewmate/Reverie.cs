61af89bf86de9cd8e786e06f3a491fac802e80f2
TronAndRey <132902063+TronAndRey@users.noreply.github.com>
2023-07-19 12:58:34 +0100

    Add files via upload

---
+++ Roles/Crewmate/Reverie.cs
@@ -0,0 +1,58 @@
+using Hazel;
+using System;
+using System.Collections.Generic;
+using static TOHE.Options;
+using System.Linq;
+using TOHE.Roles.Impostor;
+using UnityEngine;
+
+namespace TOHE;
+
+public static class Reverie
+{
+    private static readonly int Id = 1153;
+    public static List<byte> playerIdList = new();
+
+    private static OptionItem DefaultKillCooldown;
+    private static OptionItem ReduceKillCooldown;
+    private static OptionItem MinKillCooldown;
+    public static OptionItem HasImpostorVision;
+    public static OptionItem CanVent;
+
+    private static Dictionary<byte, float> NowCooldown;
+
+    public static void SetupCustomOption()
+    {
+        SetupRoleOptions(Id, TabGroup.CrewmateRoles, CustomRoles.Reverie);
+        DefaultKillCooldown = FloatOptionItem.Create(Id + 10, "SansDefaultKillCooldown", new(0f, 180f, 2.5f), 65f, TabGroup.CrewmateRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.Reverie])
+            .SetValueFormat(OptionFormat.Seconds);
+        ReduceKillCooldown = FloatOptionItem.Create(Id + 11, "SansReduceKillCooldown", new(0f, 180f, 2.5f), 15f, TabGroup.CrewmateRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.Reverie])
+            .SetValueFormat(OptionFormat.Seconds);
+        MinKillCooldown = FloatOptionItem.Create(Id + 12, "SansMinKillCooldown", new(0f, 180f, 2.5f), 2.5f, TabGroup.CrewmateRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.Reverie])
+            .SetValueFormat(OptionFormat.Seconds);
+        HasImpostorVision = BooleanOptionItem.Create(Id + 13, "ImpostorVision", true, TabGroup.CrewmateRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.Reverie]);
+        CanVent = BooleanOptionItem.Create(Id + 14, "CanVent", true, TabGroup.CrewmateRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.Reverie]);
+    }
+    public static void Init()
+    {
+        playerIdList = new();
+        NowCooldown = new();
+    }
+    public static void Add(byte playerId)
+    {
+        playerIdList.Add(playerId);
+        NowCooldown.TryAdd(playerId, DefaultKillCooldown.GetFloat());
+
+            if (!AmongUsClient.Instance.AmHost) return;
+                if (!Main.ResetCamPlayerList.Contains(playerId))
+                Main.ResetCamPlayerList.Add(playerId);
+    }
+    public static bool IsEnable => playerIdList.Count > 0;
+    public static void SetKillCooldown(byte id) => Main.AllPlayerKillCooldown[id] = NowCooldown[id];
+    public static void OnCheckMurder(PlayerControl killer)
+    {
+        NowCooldown[killer.PlayerId] = Math.Clamp(NowCooldown[killer.PlayerId] - ReduceKillCooldown.GetFloat(), MinKillCooldown.GetFloat(), DefaultKillCooldown.GetFloat());
+        killer.ResetKillCooldown();
+        killer.SyncSettings();
+    }
+}
