a0f65becaae38750f57ddcb8c4fcc092965a7522
LezaiYa <1792157206@qq.com>
2023-07-22 21:40:20 +0800

    Fix missing code

--- Roles/Impostor/Swooper.cs
+++ Roles/Impostor/Swooper.cs
@@ -3,6 +3,7 @@ using Hazel;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using TOHE.Roles.Crewmate;
 using static TOHE.Options;
 using static TOHE.Translator;
 
@@ -174,6 +175,7 @@ public static class Swooper
 
     public static bool OnCheckMurder(PlayerControl killer, PlayerControl target)
     {
+        if (Medic.ProtectList.Contains(target.PlayerId)) return true;
         if (!IsInvis(killer.PlayerId)) return true;
         killer.SetKillCooldown();
         target.RpcCheckAndMurder(target);
