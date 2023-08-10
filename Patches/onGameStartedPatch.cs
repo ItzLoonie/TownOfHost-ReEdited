b7ed3b000a7366ff9c8607490e79aa6991e3f80f
Tommy XL <fadeevvladimir16@gmail.com>
2023-08-10 23:21:58 +0800

    Lovers сan not become Sunnyboy

--- Patches/onGameStartedPatch.cs
+++ Patches/onGameStartedPatch.cs
@@ -995,6 +995,7 @@ internal class SelectRolesPatch
                 || pc.Is(CustomRoles.Dictator) 
                 || pc.Is(CustomRoles.God) 
                 || pc.Is(CustomRoles.FFF) 
+                || pc.Is(CustomRoles.Sunnyboy)
                 || pc.GetCustomRole().IsCoven()
                 || pc.Is(CustomRoles.Bomber)
                 || pc.Is(CustomRoles.Nuker) 
