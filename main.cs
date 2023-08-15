37ca7d6983204f1310cb8713d4bc4d426ddc887c
Tommy XL <fadeevvladimir16@gmail.com>
2023-08-15 19:07:49 +0800

    Add Translation

--- main.cs
+++ main.cs
@@ -33,7 +33,7 @@ public class Main : BasePlugin
     public static ConfigEntry<string> DebugKeyInput { get; private set; }
     public static readonly string MainMenuText = " ";
     public const string PluginGuid = "com.karped1em.townofhostedited";
-    public const string PluginVersion = "3.0.0.001";
+    public const string PluginVersion = "3.0.0.008";
     public const string PluginDisplayVersion = "3.0.0 dev 1";
     public const int PluginCreate = 3;
     public const bool Canary = false;
