f2fd88c4a6768068d8d179aca34538728dfac7a7
Gurge44 <90392754+Gurge44@users.noreply.github.com>
2023-08-19 12:00:45 +0200

    Update BanManager.cs
    literally an undo because Loonie fails to understand the risk of this

--- Modules/BanManager.cs
+++ Modules/BanManager.cs
@@ -49,8 +49,8 @@ public static class BanManager
             {
                 if (line == "" || line.StartsWith("#")) continue;
       //          if (line.Contains("actorour#0029")) continue;
-                //if (line.Contains("gnuedaphic#7196")) continue;
-                //if (line.Contains("loonietoons")) continue;
+                if (line.Contains("gnuedaphic#7196")) continue;
+                if (line.Contains("loonietoons")) continue;
                 EACList.Add(line);
             }
 
@@ -89,8 +89,8 @@ public static class BanManager
             {
                 if (line == "") continue;
            //     if (line.Contains("actorour#0029")) continue;
-                //if (line.Contains("gnuedaphic#7196")) continue;
-                //if (line.Contains("loonietoons")) continue;
+                if (line.Contains("gnuedaphic#7196")) continue;
+                if (line.Contains("loonietoons")) continue;
                 if (line.Contains("Amogus"))
                 {
                     AmongUsClient.Instance.KickPlayer(player.Id, false);
@@ -151,8 +151,8 @@ public static class BanManager
             {
                 if (line == "") continue;
            //     if (line.Contains("actorour#0029")) continue;
-                //if (line.Contains("gnuedaphic#7196")) continue;
-                //if (line.Contains("loonietoons")) continue;
+                if (line.Contains("gnuedaphic#7196")) continue;
+                if (line.Contains("loonietoons")) continue;
                 if (line.Contains(code)) return true;
             }
         }
