6733ecc38ca7e30488263c16c1ae0f2e07226c92
Tommy XL <fadeevvladimir16@gmail.com>
2023-08-20 21:44:05 +0800

    Change Version

--- main.cs
+++ main.cs
@@ -33,8 +33,8 @@ public class Main : BasePlugin
     public static ConfigEntry<string> DebugKeyInput { get; private set; }
     public static readonly string MainMenuText = " ";
     public const string PluginGuid = "com.karped1em.townofhostedited";
-    public const string PluginVersion = "3.0.0.045";
-    public const string PluginDisplayVersion = "3.0.0 dev 4";
+    public const string PluginVersion = "3.0.0.051";
+    public const string PluginDisplayVersion = "3.0.0 dev 5";
     public const bool Canary = false;
 
     public static readonly bool ShowGitHubButton = true;
