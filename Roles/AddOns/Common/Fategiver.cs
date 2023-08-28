5446781e42b445e975353387bb8a41ec12fdb305
NikoCat233 <139348239+NikoCat233@users.noreply.github.com>
2023-08-28 23:50:58 +0800

    delete trash code

--- Roles/AddOns/Common/Fategiver.cs
+++ Roles/AddOns/Common/Fategiver.cs
@@ -147,29 +147,10 @@ namespace TOHE.Roles.AddOns.Common
                 {
                     sum += chances[i];
                     if (randomNumber <= sum)
-                    {
-                        switch (i)
-                        {
-                            case 0:
-                                return 0;
-                            case 1:
-                                return 1;
-                            case 2:
-                                return 2;
-                            case 3:
-                                return 3;
-                            case 4:
-                                return 4;
-                            case 5:
-                                return 5;
-                            case 6:
-                                return 6;
-                            case 7:
-                                return 7;
-                        }
-                    } //Shxt codes but works fine
+                        if (i >=0 && i <= 7) 
+                            return i;
                 }
-                return 6;
+                return 6; //why would this happen
             }
         }
     }
