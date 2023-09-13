3c9043c00ed7682770d2b697256de1a5eec83dad
Tommy XL <fadeevvladimir16@gmail.com>
2023-09-13 18:32:14 +0800

    Fix GM can not see all roles && KnowRoleTraget =>KnowRoleTarget

--- Modules/Utils.cs
+++ Modules/Utils.cs
@@ -2152,7 +2152,7 @@ public static class Utils
 
                 // ====== Seer know target role ======
 
-                    string TargetRoleText = ExtendedPlayerControl.KnowRoleTraget(seer, target)
+                    string TargetRoleText = ExtendedPlayerControl.KnowRoleTarget(seer, target)
                             ? $"<size={fontSize}>{target.GetDisplayRoleName(seer.PlayerId != target.PlayerId && !seer.Data.IsDead)}{GetProgressText(target)}</size>\r\n" : "";
 
 
