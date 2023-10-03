26dae0e169a3fbc223e491f2333822064de8d588
Loonie <51543683+Loonie-Toons@users.noreply.github.com>
2023-10-03 07:21:15 -0400

    Forgot plugin version change

--- main.cs
+++ main.cs
@@ -36,7 +36,7 @@ public class Main : BasePlugin
     public static readonly string MainMenuText = " ";
 
     public const string PluginGuid = "com.karped1em.townofhostedited";
-    public const string PluginVersion = "3.1.0.142";
+    public const string PluginVersion = "3.1.0.147";
     public const string PluginDisplayVersion = "3.1.0 dev 15";
     public static readonly string SupportedVersionAU = "2023.7.11";
     public const bool Canary = false;
